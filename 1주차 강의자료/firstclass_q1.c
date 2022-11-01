c55d4ebceb91e062a8379ca3ca465e0dd63e73c6
bluebenny <benny-kim@naver.com>
2022-11-01 12:51:27 +0900

    Initial commit

---
+++ 1주차 강의자료/firstclass_q1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 0, a = 0, b = 0, c = 0;
+    double y = 0;
+
+    a = 3;
+    b = 4;
+    c = 5;
+
+    x = a * b + c;
+    printf("첫번째 출력: %d\n", x);
+
+    x = a + b * c;
+    printf("두번째 출력: %d\n", x);
+
+    y = a / b;
+    printf("세번째 출력: %f\n", y);
+
+    return 0;
+}
---
+++ 1주차 강의자료/firstclass_q2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    char sentence1[100];
+    char sentence2[100];
+
+
+    printf("문장을 입력해주세요: ");
+    gets(sentence1);
+
+    printf("문장을 입력해주세요: ");
+    scanf("%s", sentence2);
+
+
+    printf("첫번째 출력: ");
+    printf("%s\n", sentence1);
+    
+    printf("두번째 출력: ");
+    puts(sentence2);
+    
+
+    return 0;
+}
